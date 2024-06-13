ae04507a48561dddbf658a77a1b6636e1bc8d715
Mohamed-Haj-Mousa <mh661700mh@gmail.com>
2024-06-13 14:14:44 +0300

    -add br MH

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,3 +8,14 @@ abstract class Controller
 }
 //php artisan make:controller UserController
 //php artisan make:controller UserController
+
+
+
+
+
+
+//FOR BRANCH MH
+
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote')->hourly();
